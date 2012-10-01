b9ac1007922c1cbb0b0709a9c148192651297d16
Jonathan <jonathancozzo@gmail.com>
2012-10-01 17:38:54 +0200

    Développement classe "Lofteur"
    Arrêt sur la méthode "seReproduire" pas finie

--- TP Lofteurs/src/tp/lofteurs/Lofteur.java
+++ TP Lofteurs/src/tp/lofteurs/Lofteur.java
@@ -9,5 +9,30 @@ package tp.lofteurs;
  * @author jonathancozzo
  */
 public abstract class Lofteur {
+        protected int energie ;
+    protected boolean sexe ; // 0 : mec et 1 : fille
+    protected int beasoin ;
+    protected Loft loft ;
+    protected int x ;
+    protected int y ;
+    
+    public abstract void manger() ;
+    
+    public void seReproduire() {
+        int i = 0 ;
+        boolean b = false ;
+        if ( (this.energie>=5) && (this.loft.tableLofteur[x][y].size()>1)) {
+            while ((i<=this.loft.tableLofteur[x][y].size()) && (!b)) {
+                if (this.sexe = this.loft.tableLofteur[x][y].get(i).sexe) {
+                    i++ ;
+                }
+                else {
+                    b = true ;
+                    this.energie -= 5 ;
+                    this.loft.add(this.sexe, this.loft.tableLofteur[x][y]) ;
+                }
+            }
+        } 
+    }
     
 }
