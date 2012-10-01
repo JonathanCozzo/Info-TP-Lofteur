225254cdad166e98de1c26712f9ee08aed5e859c
Jonathan <jonathancozzo@gmail.com>
2012-10-01 16:49:15 +0200

    Création des classes et sous classes

---
+++ TP Lofteurs/src/tp/lofteurs/Partie.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp.lofteurs;
+
+/**
+ *
+ * @author jonathancozzo
+ */
+public class Partie {
+    
+}
---
+++ TP Lofteurs/src/tp/lofteurs/Soda.java
@@ -0,0 +1,10 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp.lofteurs;
+
+
+public class Soda extends Nourriture {
+    
+}
