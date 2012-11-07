24b5d7313f13e75431edf24d91f39de002df941f
Jonathan <jonathancozzo@gmail.com>
2012-11-07 18:23:46 +0100

    Debug Taille Fenetre

--- TP Lofteurs/src/tp/lofteurs/TPLofteurs.java
+++ TP Lofteurs/src/tp/lofteurs/TPLofteurs.java
@@ -9,13 +9,14 @@ package tp.lofteurs;
  * @author jonathancozzo
  */
 public class TPLofteurs {
-    public static int TAILLE_FENETRE_X = 600 ;
-    public static int TAILLE_FENETRE_Y = 600 ;
+
+    public static int TAILLE_FENETRE_X = 600;
+    public static int TAILLE_FENETRE_Y = 600;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //TO-DO
     }
 }
