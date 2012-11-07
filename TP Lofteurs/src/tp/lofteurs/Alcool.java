23630c62281d5c0a5d0ab69e8ba8348406258a26
Jonathan <jonathancozzo@gmail.com>
2012-11-07 18:10:58 +0100

    Alcool

--- TP Lofteurs/src/tp/lofteurs/Alcool.java
+++ TP Lofteurs/src/tp/lofteurs/Alcool.java
@@ -5,16 +5,17 @@
 package tp.lofteurs;
 
 import java.awt.Graphics;
+import java.awt.Color;
 
 public class Alcool extends Nourriture {
-    
-       public Alcool() {
-        super(-5);
+
+    public Alcool(int x, int y) {
+        super(-5, x, y); //
     }
 
-	@Override
-	public void dessinerObjet(Graphics g) {
-	// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void dessinerObjet(Graphics g) {
+        super.dessinerObjet(g);
+        g.setColor(Color.orange);
+    }
 }
